c3beb4fa44a047ba56518be0d63b2679d29e7aa2
ajkumar874134-create <ajkumar874134@gmail.com>
2026-05-07 19:31:04 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,1 +1,25 @@
+import React from "react";
 
+function Menu({ items }) {
+  return (
+    <div className="menu-container">
+      {items.map((item) => (
+        <div
+          key={item.id}
+          className="menu-item"
+          data-test-id={`menu-item-${item.category}`}
+        >
+          <img src={item.image} alt={item.title} />
+
+          <div className="menu-details">
+            <h3>{item.title}</h3>
+            <p>{item.price}</p>
+            <p>{item.category}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Menu;
